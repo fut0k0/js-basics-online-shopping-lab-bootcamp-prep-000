943b3e6988924bc18c6c66a93ae8c9153e7e0a0e
Christopher Cabell <cccabell@icloud.com>
2019-03-01 23:11:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -55,7 +55,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  
+  if (cardNumber === () {
+    return "Sorry, we don't have a credit card on file for you."
+  } else {
+    
+  }
   
   
   
