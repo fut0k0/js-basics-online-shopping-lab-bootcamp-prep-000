40f27dd99c05c60df0d14bbe211dc3768223bf57
Christopher Cabell <cccabell@icloud.com>
2019-03-01 22:14:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -38,10 +38,10 @@ function viewCart() {
 
 function total() {
   var cost = 0 
-  
-  
-  
-  
+  for (var i = 0; i < cart.length; i++) {
+    cost = cost + cart[i].itemPrice
+  }
+  return cost  
 }
 
 function removeFromCart(item) {
