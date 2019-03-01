e7ac988373b7dec216d2fbb091fa9d6db186dd58
Christopher Cabell <cccabell@icloud.com>
2019-03-01 23:00:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,7 +47,8 @@ function total() {
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
-      return cart.splice(i, 1)
+      cart.splice(i, 1)
+      return cart
     }
   }
   return "That item is not in your cart."
