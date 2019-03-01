f9cd7f4b4e9a7d1c016680380ea45f33a9296cde
Christopher Cabell <cccabell@icloud.com>
2019-03-01 22:32:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function total() {
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
-      cart.splice(i, 0)
+      cart.splice(i + 1, 0)
     }
     return cart
   }
