225b40546c14a81529204f1f600f50409b0c0943
Christopher Cabell <cccabell@icloud.com>
2019-03-01 23:19:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,44 +22,44 @@ function addToCart(item) {
 function viewCart() {
   
   if (cart.length === 0) {
-    return "Your shopping cart is empty."
+    return "Your shopping cart is empty.";
   } else if (cart.length === 1) {
-    return "In your cart, you have " + cart[0].itemName + " at $" + cart[0].itemPrice + "."
+    return "In your cart, you have " + cart[0].itemName + " at $" + cart[0].itemPrice + ".";
     }
     else {
-    var list = []
+    var list = [];
     for (var i = 0; i < cart.length - 1; i++ ) {
-      list[i] = " " + cart[i].itemName + " at $" + cart[i].itemPrice
+      list[i] = " " + cart[i].itemName + " at $" + cart[i].itemPrice;
     }
-    list[cart.length - 1] = " and " + cart[cart.length -1].itemName + " at $" + cart[cart.length - 1].itemPrice + "."
-    return "In your cart, you have" + list
+    list[cart.length - 1] = " and " + cart[cart.length -1].itemName + " at $" + cart[cart.length - 1].itemPrice + ".";
+    return "In your cart, you have" + list;
     }
 }
 
 function total() {
-  var cost = 0 
+  var cost = 0;
   for (var i = 0; i < cart.length; i++) {
-    cost = cost + cart[i].itemPrice
+    cost = cost + cart[i].itemPrice;
   }
-  return cost  
+  return cost;
 }
 
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
-      cart.splice(i, 1)
-      return cart
+      cart.splice(i, 1);
+      return cart;
     }
   }
-  return "That item is not in your cart."
+  return "That item is not in your cart.";
 }
 
 function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
-    return "Sorry, we don't have a credit card on file for you."
+    return "Sorry, we don't have a credit card on file for you.";
   } else {
-    var cost = total()
-    cart = []
-    return "Your total cost is $" + cost + ", which will be charged to the card " + cardNumber + "."
+    var cost = total();
+    cart = [];
+    return "Your total cost is $" + cost + ", which will be charged to the card " + cardNumber + ".";
   }
 }
